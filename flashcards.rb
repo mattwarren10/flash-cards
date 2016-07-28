e965cdfe767bac83e36450a52e2a717d9afa5e70
Matt Warren <mwarren@harding.edu>
2016-07-27 21:59:06 -0400

    instantiate all flashcards, add them to flashcard_stack

--- flashcards.rb
+++ flashcards.rb
@@ -1,2 +1,18 @@
-class Definition
+class FlashcardStack
+	def initialize
+		@stack = []
+	end
+
+	def add_flashcards(f)
+		@stack.push(f)
+	end
+
+	
+end
+class Flashcard
+	attr_accessor :definition, :term
+	def initialize(definition, term)
+		@definition = definition
+		@term = term
+	end
 end
